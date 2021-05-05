72b17f4a87b1ddb1e84ba064c76c74f8f4bf4338
fourastiemarvin <marvin.fourastie@etu.unige.ch>
2021-05-05 18:14:32 +0200

    add cell encryption and decryption

--- OR.py
+++ OR.py
@@ -5,6 +5,7 @@ from cell import *
 import pyDH
 import time
 from tools import *
+import ast
 
 class OR(threading.Thread):
 
@@ -22,15 +23,35 @@ class OR(threading.Thread):
         self.sharedKey = []
 
     # send cell to an OR
-    def sendCell(self, cell, newOR):
+    def sendCell(self, cell, newOR, toForward=False):
         if self.isConnected == False:
             self.sockOut.connect(("", newOR.portIn))
             self.isConnected = True
         self.portOut = self.sockOut.getsockname()[1]
-        # if cell.command == extend
-        self.sockOut.sendall(pickle.dumps(cell))
-        print("\n %s have sent <%s> cell" % (self.name, cell.command))
-
+        if toForward:
+            self.sockOut.sendall(cell)
+        else:
+            try:
+                if cell.command == "create":
+                    self.sockOut.sendall(pickle.dumps(cell))
+                    print("\n %s have sent <%s> cell" % (self.name, cell.command))
+                else:
+                    cellToSend = cell
+                    # "wrap" with all the shared keys
+                    for i in range(len(self.sharedKey)):
+                        j = (len(self.sharedKey) - 1) - i
+                        cellToSend = pickle.dumps(encryptDecrypt(str(pickle.dumps(cellToSend)), self.sharedKey[j]))
+                        # self.sockOut.sendall(pickle.dumps(encryptDecrypt(str(pickle.dumps(cell)), self.sharedKey[i])))
+                    self.sockOut.sendall(cellToSend)
+                    print("\n %s have sent %s" % (self.name, cell))
+            except:
+                cellToSend = cell
+                # "wrap" with all the shared keys
+                for i in range(len(self.sharedKey)):
+                    j = (len(self.sharedKey) - 1) - i
+                    cellToSend = pickle.dumps(encryptDecrypt(str(pickle.dumps(cellToSend)), self.sharedKey[j]))
+                self.sockOut.sendall(cellToSend)
+                print("\n %s have sent %s" % (self.name, cell))
     # listen to a random port (simulate dynamic IP adress)
     def run(self):
         self.sockIn.bind(("", 0))
@@ -40,10 +61,20 @@ class OR(threading.Thread):
         conn, addr = self.sockIn.accept()
         while True:
             time.sleep(1)
-            data = conn.recv(1024)
+            data = conn.recv(4096)
             dataObj = pickle.loads(data)
             self.recvCell = str(dataObj)
-            print("\n %s have received <%s> cell" % (self.name, dataObj.command))
-            dataObj.execute(self)
+            try:
+                dataObj.command
+            except:
+                tmp = encryptDecrypt(dataObj, self.sharedKey[0])
+                dataObj = pickle.loads(ast.literal_eval(tmp))
+            if type(dataObj) == Cell:
+                dataObj.execute(self)
+                print("\n %s have received <%s> cell" % (self.name, dataObj.command))
+            else:
+                cellToSend = pickle.dumps(encryptDecrypt(str(pickle.dumps(dataObj)), self.sharedKey[0]))
+                self.sendCell(cellToSend, self.exitOR, toForward=True)
+                print("\n %s receive cell to forward" % (self.name))
             if not data:
                 break
--- run.py
+++ run.py
@@ -18,41 +18,38 @@ relay1 = OR("relay1")
 bob = OR("Bob")
 alice.exitOR = relay1
 relay1.exitOR = bob
-#
+
 alice.start()
 relay1.start()
 bob.start()
-
+# alice encrypt n times regarding to the n nodes to the destination (extend)
 time.sleep(1)
 alice.sendCell(Cell("create",{"pubKey":alice.pubKey}), relay1)
 # compute relay1 sharedKey
 alice.sharedKey.append(alice.dh.gen_shared_key(relay1.pubKey))
-#
+
 time.sleep(1)
-# alice.sendCell(Cell("extend", {"hop":1, "pubKey":None}), relay1)
-# # compute bob sharedKey
-# relay1.sharedKey.append(relay1.dh.gen_shared_key(bob.pubKey))
-#
-# # time.sleep(1)
-# # compute bob sharedKey
-# # alice.sharedKey.append(alice.dh.gen_shared_key(bob.pubKey))
-# time.sleep(3)
+alice.sendCell(Cell("extend", {"hop":1, "pubKey":alice.pubKey, "destination":bob.name}), relay1)
+# compute bob sharedKey -> he knows only the alice shared key (without knowing it is the alice one)
+relay1.sharedKey.append(relay1.dh.gen_shared_key(bob.pubKey))
+alice.sharedKey.append(alice.dh.gen_shared_key(bob.pubKey))
+
+# time.sleep(1)
 # print(alice.sharedKey)
 # print(relay1.sharedKey)
 # print(bob.sharedKey)
-# print(relay1.sharedKey[-1] == bob.sharedKey[-1])
 
-print("------TESTS-------")
+# print("------TESTS-------")
 # alice.sendCell(encryptDecrypt(str(Cell("create",{"pubKey":alice.pubKey})), alice.sharedKey[0]), relay1)
-print(pickle.dumps(Cell("create",{"pubKey":None})))
-a = encryptDecrypt(str(pickle.dumps(Cell("create",{"pubKey":None}))), alice.sharedKey[0])
-print(a + "\n")
-b = encryptDecrypt(a, alice.sharedKey[0])
-print(b)
-print(type(b))
-print(type(ast.literal_eval(b)))
-print(pickle.loads(ast.literal_eval(b)))
-print(pickle.loads(ast.literal_eval(b)).command)
+# print(pickle.dumps(Cell("create",{"pubKey":None})))
+# a = encryptDecrypt(str(pickle.dumps(Cell("create",{"pubKey":None}))), alice.sharedKey[0])
+# print(a + "\n")
+# b = encryptDecrypt(a, alice.sharedKey[0])
+# print(b)
+# print(type(b))
+# print(type(ast.literal_eval(b)))
+# print(pickle.loads(ast.literal_eval(b)))
+# print(pickle.loads(ast.literal_eval(b)).command)
 
 # print(encryptDecrypt(a, alice.sharedKey[0]))
 
